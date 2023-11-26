56bdd2becc5158440435752e3f63b810c978f99b
Galaxeo <hwjustincheok@gmail.com>
2023-11-26 16:56:04 -0500

    feat: user and staff register done

--- init1.py
+++ init1.py
@@ -1,6 +1,7 @@
 #Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
+import hashlib
 
 #Initialize the app from Flask
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app = Flask(__name__)
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
-                       db='blog',
+                       db='airport_system',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
@@ -27,6 +28,77 @@ def login():
 @app.route('/register')
 def register():
 	return render_template('register.html')
+
+@app.route('/userRegister')
+def userRegister():
+	return render_template('userRegister.html')
+
+@app.route("/staffRegister")
+def staffRegister():
+	return render_template('staffRegister.html')
+
+#Authenticate login for users
+@app.route('/userRegisterAuth', methods=['GET', 'POST'])
+def userRegisterAuth():
+	#TODO: if aptNum is missing, insert NULL
+	#grabs information from the forms
+	email = request.form['email']
+	password = request.form['password']
+	firstName = request.form['firstname']
+	lastName = request.form['lastname']
+	buildingNum= request.form['buildingNum']
+	street = request.form['street']
+	aptNum = request.form['aptNum']
+	city = request.form['city']
+	state= request.form['state']
+	zipcode = request.form['zipcode']
+	passportNumber = request.form['passportNumber']
+	passportExpiration = request.form['passportExpiration']
+	passportCountry = request.form['passportCountry']
+	dateOfBirth = request.form['dateOfBirth']
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM customer WHERE email = %s'
+	cursor.execute(query, (email))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	error = None
+	if(data):
+		#If the previous query returns data, then user exists
+		error = "This user already exists"
+		return render_template('userRegister.html', error = error)
+	else:
+		ins = 'INSERT INTO customer VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s,%s, %s)'
+		cursor.execute(ins, (email, password, firstName, lastName, buildingNum, street, aptNum, city, state, zipcode, passportNumber, passportExpiration, passportCountry, dateOfBirth))
+		conn.commit()
+		cursor.close()
+		return render_template('index.html')
+
+#Authenticate login for staff
+@app.route('/staffRegisterAuth', methods=['GET', 'POST'])
+def staffRegisterAuth():
+	username = request.form['username']
+	password = request.form['password']
+	airlineName = request.form['airlineName']
+	firstName = request.form['firstname']
+	lastName = request.form['lastname']
+	dateOfBirth = request.form['dateOfBirth']
+	cursor = conn.cursor()
+	query = 'SELECT * FROM airlinestaff WHERE username = %s and password = %s'
+	cursor.execute(query, (username, password))
+	data = cursor.fetchone()
+	error = None
+	if(data):
+		error = "This user already exists"
+		return render_template('staffRegister.html', error = error)
+	else:
+		ins = 'INSERT INTO airlinestaff VALUES(%s, %s, %s, %s, %s, %s)'
+		cursor.execute(ins, (username, password, airlineName, firstName, lastName, dateOfBirth))
+		conn.commit()
+		cursor.close()
+		return render_template('index.html')
 
 #Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
@@ -81,6 +153,15 @@ def registerAuth():
 		conn.commit()
 		cursor.close()
 		return render_template('index.html')
+
+@app.route('/showFlights', methods=['GET', 'POST'])
+def showFlights():
+	cursor = conn.cursor();
+	query = 'SELECT * FROM `flight`'
+	cursor.execute(query)
+	data = cursor.fetchall()
+	cursor.close()
+	return render_template('index.html', data=data)
 
 @app.route('/home')
 def home():
